d644a52bbd69a279cc75e7b87d2e7b7484736f28
Serigne Fallou Mbacke Sall <bigfallou99@gmail.com>
2024-12-18 14:52:36 +0000

    Update ASIE_AIRLINE.py

--- ASIE_AIRLINE.py
+++ ASIE_AIRLINE.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 from os import sep
 st.title(" Indian Domestic Airline Data Analysis")
-path=("Indian_Domestic_Airline.csv")
+path=("Indian_Domestic_Airline.csv", encoding='utf-8')
 df=pd.read_csv(path)
 #df.head()
 st.dataframe(df)
